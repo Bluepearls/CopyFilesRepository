6f1bb7db8eece8384a5d694bf7cc6b68d23d478a
Pearl Liang <pearl.liang@flex.com>
2020-06-23 14:17:28 +0800

    1.0.0.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CopyFiles")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CopyFiles1.0.0.2")]
+[assembly: AssemblyDescription("CopyFiles")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CopyFiles")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
